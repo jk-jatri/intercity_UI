c266907568351fc943f629b875acd72f67b1e8ba
joyonto <joyonto.jatri@gmail.com>
2021-02-15 18:13:56 +0600

    joyonto: 20% done

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,4 @@
+// Login page password Hide/Show
 $(".toggle-password").click(function () {
   $(this).toggleClass("fa-eye fa-eye-slash");
   var input = $($(this).attr("toggle"));
@@ -7,3 +8,20 @@ $(".toggle-password").click(function () {
     input.attr("type", "password");
   }
 });
+
+
+
+// table view details
+
+const view = document.getElementById('view_button');
+const hiddenRow = document.getElementById('hide_row');
+const buttonText = document.getElementById('button_text');
+view.addEventListener('click', function(){
+  hiddenRow.classList.toggle('d-block');
+  if(buttonText.innerHTML === "View Seat"){
+    buttonText.innerHTML = "Hide Seat";
+  }
+  else{
+    buttonText.innerHTML = "View Seat";
+  }
+})
